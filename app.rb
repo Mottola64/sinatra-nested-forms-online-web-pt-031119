7da0320b00201aa0c2a331383ddf643c701160c9
Michael Mottola <mottola64@gmail.com>
2019-06-24 00:39:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,17 +3,9 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    get '/' do
-      erb :root
-    end
+  get '/new' do
+    erb :new
+  end
 
-    get '/new' do
-      erb :new
-    end
-
-    post '/pirates' do
-
-      erb :show
-    end
   end
 end
